b88de4394cd3bdad366b5e293d1f72a3c6bb9b46
ayushbadrakiya <ayushbadrakiya@gmail.com>
2026-01-16 18:49:26 +0530

    final

--- server/index.js
+++ server/index.js
@@ -37,6 +37,8 @@ const EMAIL_PASS = process.env.EMAIL_PASS;
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+  port:587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
